03e9b6ca45f4eeac465e73db166b7c829457c779
Golden Techie <goldentechie3@gmail.com>
2021-12-14 22:23:09 -0500

    certificate problem

--- backend/app/Http/Controllers/SlackAPIController.php
+++ backend/app/Http/Controllers/SlackAPIController.php
@@ -3,8 +3,47 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use stdClass;
+use App\Models\Events;
+use App\Models\Config;
+use App\Models\Channels;
+use App\Models\TeamUsers;
 
 class SlackAPIController extends Controller
 {
     //
+    public function refresh(Request $req)
+    {
+        $response = Http::withOptions(['verify'=>false,'proxy' => 'http://rgs0712:likiseng1207@192.168.91.120:80'])
+        ->post('https://slack.com/api/conversations.history', [
+            'token' => 'xoxp-2732118217091-2717446413447-2830726595763-61082609021a76c8a5d96892f6525040',
+            'channel' => 'C02MJ2ZSRTL',
+            'limit' => 1000
+        ]);
+        
+        var_dump($response);
+        return $response->json();
+    }
+
+    public function getEvents()
+    {
+        $a = new stdClass;
+        $a->a = 'a';
+        $a->b = 'b';
+        return $a;
+    }
+    public function getChannels()
+    {
+        $config = Config::all()->toArray();
+        return $config;
+    }
+    public function getMessages()
+    {
+
+    }
+    public function getFiles()
+    {
+
+    }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SlackAPIController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,14 +15,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 // rescan whole data from slack
-Route::post("refresh", "SlackAPIController@refresh");
-
-Route::post("getEvents", "SlackAPIController@getEvents");
-Route::post("getChannels", "SlackAPIController@getChannels");
-Route::post("getMessages", "SlackAPIController@getMessages");
-Route::post("getFiles", "SlackAPIController@getFiles");
+// Route::post("refresh", function() {
+//     return "A";
+// });
+Route::prefix('slack')->group(function () {
+    Route::post("refresh", [SlackAPIController::class, 'refresh']);
+    Route::post("getEvents", [SlackAPIController::class, 'getEvents']);
+    Route::post("getChannels", [SlackAPIController::class, 'getChannels']);
+    Route::post("getMessages", [SlackAPIController::class, 'getMessages']);
+    Route::post("getFiles", [SlackAPIController::class, 'getFiles']);
+});
