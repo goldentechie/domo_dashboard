18dbb8616673d138d99cd3071807ed644960a4a6
Golden Techie <goldentechie3@gmail.com>
2021-12-16 16:19:22 -0500

    api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SierraAPIController;
 use App\Http\Controllers\SlackAPIController;
 use Illuminate\Http\Request;
@@ -25,6 +26,8 @@ use App\Http\Controllers\SlackDashboardAPIController;
 // Route::post("refresh", function() {
 //     return "A";
 // });
+
+// Front-end api
 Route::prefix('dashboard')->group(function () {
     Route::post("refresh", [SlackDashboardAPIController::class, 'refresh']);
     Route::post("getEvents", [SlackDashboardAPIController::class, 'getEvents']);
@@ -32,12 +35,17 @@ Route::prefix('dashboard')->group(function () {
     Route::post("getMessages", [SlackDashboardAPIController::class, 'getMessages']);
     Route::post("getFiles", [SlackDashboardAPIController::class, 'getFiles']);
     Route::post("test", [SlackDashboardAPIController::class, 'test']);
+
+    // redirecting api for register team
+    Route::get('redirect', [RegisterController::class, 'index']);
 });
 
+// slack interactive api when a new lead is claimed on slack channel
 Route::prefix('slack')->group(function () {
     Route::post("event", [SlackAPIController::class, 'index']);
 });
 
+// sierra triggerd event when a new lead created
 Route::prefix('sierra')->group(function () {
     Route::post("event", [SierraAPIController::class, 'index']);
 });
